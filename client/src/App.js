c76ce75307595e1ec0749f37cfcb056bf8a3aac0
HemantDutta <hemantnorddutta2@gmail.com>
2023-08-08 23:38:09 +0530

    Create React App | Cleanup | Create CSS File

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {Navbar} from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+          <Routes>
+              <Route path={"/"} element={<Navbar/>}/>
+          </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,15 @@
+export const Navbar = () => {
+    return(
+        <>
+            <nav>
+                <div className="navbar-inner-container">
+                    <div className="navbar-logo">
+                        <span className="accent-color">one</span><span>Health</span>
+                    </div>
+                    <div className="navbar-links"></div>
+                    <div className="navbar-join-btn"></div>
+                </div>
+            </nav>
+        </>
+    )
+}
