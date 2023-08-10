94898e66250cbf6cabe6255d573a822f059b1483
HemantDutta <hemantnorddutta2@gmail.com>
2023-08-10 15:25:23 +0530

    Create Join Now | Login Form

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,43 +1,89 @@
 import {Link} from "react-router-dom";
-import {useEffect} from "react";
+import {useEffect, useRef, useState} from "react";
 
-export const Navbar = () => {
+export const Navbar = ({joinNowTrigger}) => {
+
+    //States
+    const [serviced, setServiced] = useState(0);
+
+    //Refs
+    const firstRender = useRef(true);
 
     //Big Menu Functions
-    function toggleBigMenu(){
+    function toggleBigMenu() {
         let ham = document.getElementById("hamburger");
         let bigMenu = document.getElementById("bigMenu");
 
-        if(window.innerWidth<991){
-            if(ham.classList.contains("clicked")){
+        if (window.innerWidth < 991) {
+            if (ham.classList.contains("clicked")) {
                 ham.classList.remove("clicked");
                 bigMenu.classList.remove("opened");
-                setTimeout(()=>{
+                setTimeout(() => {
                     bigMenu.style.display = "none";
-                },200)
-            }
-            else{
+                }, 200)
+            } else {
                 ham.classList.add("clicked");
                 bigMenu.style.display = "initial";
-                setTimeout(()=>{
+                setTimeout(() => {
                     bigMenu.classList.add("opened");
-                },200)
+                }, 200)
+            }
+        }
+    }
+
+    //Toggle Join Now
+    function toggleJoinNow() {
+        let jn = document.getElementById("joinNow");
+        let body = document.getElementsByTagName("body")[0];
+        if (jn.classList.contains("opened")) {
+            jn.classList.remove("opened");
+            setTimeout(() => {
+                jn.style.display = "none";
+                body.style.overflowY = "auto";
+            }, 400)
+        } else {
+            jn.style.display = "grid";
+            body.style.overflowY = "hidden";
+            setTimeout(() => {
+                jn.classList.add("opened");
+            }, 100)
+        }
+    }
+
+    //Call Join Now from Home Component
+    useEffect(() => {
+        if (firstRender.current) {
+            firstRender.current = false;
+        } else if (serviced < joinNowTrigger) {
+            console.log("Call From Home");
+            toggleJoinNow();
+            setServiced(joinNowTrigger);
+        }
+    }, [joinNowTrigger])
+
+    //Click away to close Join Now
+    function clickAwayJoinNow(e) {
+        let jnc = document.getElementById("jn-content");
+        let jn = document.getElementById("joinNow");
+        if (jn.classList.contains("opened")) {
+            if (!jnc.contains(e.target)) {
+                toggleJoinNow();
             }
         }
     }
 
     //Resize Listener for bigMenu
-    useEffect(()=>{
+    useEffect(() => {
 
-        const resizeBigMenu = () =>{
-            if(window.innerWidth>991){
+        const resizeBigMenu = () => {
+            if (window.innerWidth > 991) {
                 let ham = document.getElementById("hamburger");
                 let bigMenu = document.getElementById("bigMenu");
                 ham.classList.remove("clicked");
                 bigMenu.classList.remove("opened");
-                setTimeout(()=>{
+                setTimeout(() => {
                     bigMenu.style.display = "none";
-                },200)
+                }, 200)
             }
         }
 
@@ -47,10 +93,10 @@ export const Navbar = () => {
             window.removeEventListener("resize", resizeBigMenu);
         }
 
-    },[])
+    }, [])
 
 
-    return(
+    return (
         <>
             <nav>
                 <div className="navbar-inner-container">
@@ -63,7 +109,7 @@ export const Navbar = () => {
                         <Link to={"/"}>About</Link>
                     </div>
                     <div className="navbar-join-btn">
-                        <button>Join Now</button>
+                        <button onClick={toggleJoinNow}>Join Now</button>
                     </div>
                     <div className="navbar-hamburger-option">
                         <div className="hamburger" id="hamburger" onClick={toggleBigMenu}>
@@ -73,23 +119,63 @@ export const Navbar = () => {
                 </div>
             </nav>
             <div className="bigMenu" id="bigMenu">
-                <div className="bigMenu-container">
+                <div className="bigMenu-container head-font">
                     <Link className="bigMenu-item">
                         <div className="item-text">Dashboard</div>
-                        <div className="item-icon"></div>
+                        <div className="item-icon">
+                            <i className="fa-solid fa-arrow-right"/>
+                        </div>
                     </Link>
-                    <Link className="bigMenu-item">
+                    <span className="bigMenu-item" onClick={() => {
+                        toggleBigMenu();
+                        toggleJoinNow();
+                    }}>
                         <div className="item-text">Join Now</div>
-                        <div className="item-icon"></div>
-                    </Link>
+                        <div className="item-icon">
+                            <i className="fa-solid fa-arrow-right"/>
+                        </div>
+                    </span>
                     <Link className="bigMenu-item">
                         <div className="item-text">Diagnose</div>
-                        <div className="item-icon"></div>
+                        <div className="item-icon">
+                            <i className="fa-solid fa-arrow-right"/>
+                        </div>
                     </Link>
                     <Link className="bigMenu-item">
                         <div className="item-text">About</div>
-                        <div className="item-icon"></div>
+                        <div className="item-icon">
+                            <i className="fa-solid fa-arrow-right"/>
+                        </div>
                     </Link>
+                </div>
+            </div>
+            <div className="joinNow" id="joinNow" onClick={clickAwayJoinNow}>
+                <div className="joinNow-content" id="jn-content">
+                    <div className="joinNow-login">
+                        <div className="login-form-header head-font">
+                            <span>Welcome back!</span>
+                        </div>
+                        <div className="login-form-tag">
+                            <span>Sign in to continue to oneHealth</span>
+                        </div>
+                        <div className="login-form">
+                            <form>
+                                <div className="input-field">
+                                    <label htmlFor="email"><i className="fa-regular fa-envelope"/> Email Address</label>
+                                    <input type="email" id="email"/>
+                                </div>
+                                <div className="input-field">
+                                    <label htmlFor="password"><i className="fa-solid fa-key"/> Password</label>
+                                    <input type="password" id="password"/>
+                                </div>
+                                <div className="btn-field">
+                                    <button className="hover-btn">Log in</button>
+                                    <span>Not a member yet?</span>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                    <div className="joinNow-register"></div>
                 </div>
             </div>
         </>
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,43 @@
 import {Navbar} from "../components/Navbar";
-
+import {useState} from "react";
 export const Home = () => {
+
+    //States
+    const [joinNowTrigger, setJoinNowTrigger] = useState(0);
+
     return(
         <>
-            <Navbar/>
+            <Navbar joinNowTrigger={joinNowTrigger}/>
             <div className="home-container">
                 <section className="hero" id="hero">
+                    <div className="hero-content">
+                        <div className="hero-left">
+                            <div className="hero-left-head head-font">
+                                <span><span style={{color: "#5da1bd"}}>Brain Tumours</span> or <span style={{color: "#6bacad"}}>Heart Diseases.</span></span>
+                                <span>We have got you covered.</span>
+                            </div>
+                            <div className="hero-left-tag">
+                                <span>For quick and reliable diagnosis and predictions, join our platform now!</span>
+                            </div>
+                            <div className="hero-left-cta">
+                                <button className="hover-btn" onClick={()=>{setJoinNowTrigger((joinNowTrigger)=>joinNowTrigger+1)}}>Join Now</button>
+                            </div>
+                        </div>
+                        <div className="hero-right">
+                            <div className="hero-right-img">
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <section className="model-display">
+                    <div className="model-display-container">
+                        <div className="model-display-header">
+                            <span>Our Models</span>
+                        </div>
+                        <div className="model-display-scroller">
 
+                        </div>
+                    </div>
                 </section>
             </div>
         </>
