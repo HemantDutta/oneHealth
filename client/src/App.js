7e23ce17ac8319b55afa741c8cef46975695a046
HemantDutta <hemantnorddutta2@gmail.com>
2023-08-11 11:00:41 +0530

    Complete Hero | Complete Brain Model

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import {Home} from "./pages/Home";
+import {Diagnose} from "./pages/Diagnose";
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <BrowserRouter>
           <Routes>
               <Route path={"/"} element={<Home/>}/>
+              <Route path={"/diagnose"} element={<Diagnose/>}/>
           </Routes>
       </BrowserRouter>
     </>
